91f2542f31ecb092cf26cfe38e023f1d18618601
Witold Krzyk <witoldkrzyk@Witolds-MacBook-Air.local>
2022-04-16 02:23:25 +0200

    add txt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from typing import List
-from uuid import uuid4
-from fastapi import FastAPI
-
+from uuid import uuid4, UUID
+from fastapi import FastAPI, HTTPException
 from models import User, Gender, Role
 
 app = FastAPI()
@@ -38,3 +37,15 @@ async def fetch_users():
 async def register_users(user: User):
     db.append(user)
     return {"id": user.id}
+
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return
+        raise HTTPException(
+            status_code=404,
+            detail=f"user with id: {user_id} doesn't exists"
+        )
